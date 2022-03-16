789a5175ab78aea05d338321c44c251e2ff7d6b0
Marwen - JAMMELI <marwen.jammeli@gmail.com>
2022-03-17 00:38:17 +0100

    Transfer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import React1 from "./React.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+
+                <h1 className="title red">React JS</h1>
+
+                <br />
+
+                <img src="/ReactJs.jpg" style={{width: "25%"}} />
+
+                <br />
+
+                <img src={React1} style={{width: "25%"}} />
+
+      </div>
+
+                <video style={{width:"320px",height:"240px"}} controls>
+
+                <source src="trailer.mp4" type="video/mp4" />
+
+                </video>
+       
     </div>
+ 
   );
 }
 
